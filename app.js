cc7b8a0f7e086d4567fac54c1bc12444444e079b
ALPER <icmyagmuracikgoz@gmail.com>
2024-08-28 12:50:38 +0300

    add nodemon etc.

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
-const fn = function () {
-  console.log('Function working');
-};
+const express= require("express");
+
+const app = express();
+
+
+app.get('/', (req,res)=>{
+
+    const photo= {
+        id:1,
+        name:"Photo name",
+        description:"Photo description"
+    }
+    res.send(photo)
+});
+
+const port = 3000;
+app.listen(port,()=>{
+    console.log(`Sunucu ${port} portunda başlatıldı..`)
+});
